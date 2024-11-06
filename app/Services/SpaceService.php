5036957c09102c95b1a9b061ff0bb5e1e50fcc01
ludks1 <ludwindrts@gmail.com>
2024-11-06 16:44:15 -0600

    Qr function generated

--- app/Services/SpaceService.php
+++ app/Services/SpaceService.php
@@ -7,6 +7,21 @@ use Illuminate\Database\Eloquent\Collection;
 
 class SpaceService
 {
+
+    public function createQRCode(Space $space)
+    {
+        $jsonData = $space->toJson();
+
+        $qrCodeImg = Space::format('png')
+            ->size(200)
+            ->generate($jsonData);
+        $qrCodeBase64 = base64_encode($qrCodeImg);
+
+        $space->qrCode = $qrCodeBase64;
+        $space->save();
+
+        return $qrCodeBase64;
+    }
     public function createSpace(array $data): Space
     {
         return Space::create([
